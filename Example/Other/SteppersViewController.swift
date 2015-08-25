b307ee6f1853e7ff4b7be01bcfc5f94dab6a8d27
Simon Strandgaard <neoneye@gmail.com>
2015-08-25 17:46:15 +0200

    Made a bigger example page showing the stepper control. Thank you @tehprofessor for this brilliant idea.

---
+++ Example/Other/SteppersViewController.swift
@@ -0,0 +1,37 @@
+//  Copyright © 2015 Simon Strandgaard. All rights reserved.
+import UIKit
+import SwiftyFORM
+
+class SteppersViewController: FormViewController {
+	
+	override func populate(builder: FormBuilder) {
+		builder.navigationTitle = "Steppers"
+		builder += SectionHeaderTitleFormItem().title("Animals seen")
+		builder += catStepper
+		builder += dogStepper
+		builder += submitButton
+	}
+	
+	lazy var catStepper: StepperFormItem = {
+		let instance = StepperFormItem()
+		instance.title = "Number of Cats"
+		return instance
+		}()
+	
+	lazy var dogStepper: StepperFormItem = {
+		let instance = StepperFormItem()
+		instance.title = "Number of Dogs"
+		return instance
+		}()
+	
+	lazy var submitButton: ButtonFormItem = {
+		let instance = ButtonFormItem()
+		instance.title = "Submit"
+		instance.action = { [weak self] in
+			let catCount = self?.catStepper.value ?? 0
+			let dogCount = self?.dogStepper.value ?? 0
+			self?.form_simpleAlert("Submit", "There are \(catCount) cats and \(dogCount) dogs!")
+		}
+		return instance
+		}()
+}
