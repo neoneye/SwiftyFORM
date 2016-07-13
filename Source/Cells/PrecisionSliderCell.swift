7e5f40e10f6edb42f96e6a05178ebf06f29419c9
Simon Strandgaard <neoneye@gmail.com>
2016-07-13 21:51:19 +0200

    Imported precision slider component from ScientificSliderViewController2

--- Source/Cells/PrecisionSliderCell.swift
+++ Source/Cells/PrecisionSliderCell.swift
@@ -1,6 +1,246 @@
 // MIT license. Copyright (c) 2016 SwiftyFORM. All rights reserved.
 import UIKit
 
+class PrecisionSlider_InnerModel {
+	var count = 1000
+	var scale: CGFloat = 60.0
+	
+	var scaleRounded: CGFloat {
+		let result = floor(scale + 0.5)
+		if result < 0.1 {
+			return 0.1
+		}
+		return result
+	}
+	
+	static let height: CGFloat = 130
+}
+
+class PrecisionSlider_InnerCollectionViewFlowLayout: UICollectionViewFlowLayout {
+	weak var model: PrecisionSlider_InnerModel?
+	
+	override func collectionViewContentSize() -> CGSize {
+		guard let model = self.model else {
+			print("no model")
+			return CGSizeZero
+		}
+		return CGSize(width: model.scaleRounded * CGFloat(model.count), height: PrecisionSlider_InnerModel.height)
+	}
+}
+
+class PrecisionSlider_InnerCollectionViewCell: UICollectionViewCell {
+	static let identifier = "cell"
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		commonInit()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		commonInit()
+	}
+	
+	func commonInit() {
+		backgroundColor = UIColor.whiteColor()
+		addSubview(leftBorder)
+		addSubview(label)
+	}
+	
+	lazy var leftBorder: UIView = {
+		let instance = UIView()
+		instance.backgroundColor = UIColor.blackColor()
+		return instance
+	}()
+	
+	lazy var label: UILabel = {
+		let instance = UILabel()
+		instance.text = "0"
+		return instance
+	}()
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		leftBorder.frame = CGRect(x: 0, y: 0, width: 1, height: bounds.height)
+		
+		let labelHidden = self.bounds.width < 30
+		label.hidden = labelHidden
+		
+		label.sizeToFit()
+		let labelFrame = label.frame
+		label.frame = CGRect(x: 7, y: 5, width: bounds.width - 10, height: labelFrame.height)
+	}
+}
+
+class PrecisionSliderView: UIView, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UIScrollViewDelegate, UIGestureRecognizerDelegate {
+	var originalScale: CGFloat = 1.0
+	var originalValue: CGFloat?
+	
+	typealias ValueDidChange = Void -> Void
+	var valueDidChange: ValueDidChange?
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		commonInit()
+	}
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		commonInit()
+	}
+	
+	func commonInit() {
+		addSubview(collectionView)
+		addSubview(leftCoverView)
+		addSubview(rightCoverView)
+	}
+	
+	func viewWillAppear() {
+		addGestureRecognizer(pinchGestureRecognizer)
+		
+		layout.itemSize = computeItemSize()
+		layout.invalidateLayout()
+		collectionView.reloadData()
+	}
+	
+	func viewDidDisappear() {
+		removeGestureRecognizer(pinchGestureRecognizer)
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		collectionView.frame = bounds
+		
+		let halfWidth = round(bounds.width/2)-1
+		collectionView.contentInset = UIEdgeInsets(top: 0, left: halfWidth, bottom: 0, right: halfWidth)
+		
+		let (leftFrame, rightFrame) = bounds.divide(round(bounds.width/2), fromEdge: .MinXEdge)
+		leftCoverView.frame = CGRect(x: leftFrame.origin.x, y: leftFrame.origin.y, width: leftFrame.size.width - 1, height: leftFrame.size.height)
+		rightCoverView.frame = rightFrame
+	}
+	
+	lazy var leftCoverView: UIView = {
+		let instance = UIView()
+		instance.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+		instance.userInteractionEnabled = false
+		return instance
+	}()
+	
+	lazy var rightCoverView: UIView = {
+		let instance = UIView()
+		instance.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
+		instance.userInteractionEnabled = false
+		return instance
+	}()
+	
+	lazy var model: PrecisionSlider_InnerModel = {
+		let instance = PrecisionSlider_InnerModel()
+		return instance
+	}()
+	
+	var value: CGFloat? {
+		let scale = model.scaleRounded
+		if scale < 0.1 {
+			return nil
+		}
+		
+		let halfWidth = collectionView.bounds.width / 2
+		let midX = collectionView.contentOffset.x + halfWidth
+		let x: CGFloat = midX / scale
+		return x
+	}
+	
+	func scrollToValue(value: CGFloat) {
+		let scale = model.scaleRounded
+		if scale < 0.1 {
+			return
+		}
+		
+		let halfWidth = collectionView.bounds.width / 2
+		let offsetX: CGFloat = round((scale * value) - halfWidth)
+		collectionView.setContentOffset(CGPoint(x: offsetX, y: 0), animated: false)
+	}
+	
+	lazy var pinchGestureRecognizer: UIPinchGestureRecognizer = {
+		let instance = UIPinchGestureRecognizer(target: self, action: #selector(PrecisionSliderView.handlePinch))
+		instance.delegate = self
+		return instance
+	}()
+	
+	func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+		return true
+	}
+	
+	func handlePinch(gesture: UIPinchGestureRecognizer) {
+		if gesture.state == .Began {
+			originalScale = model.scale
+			originalValue = self.value
+		}
+		if gesture.state == .Changed {
+			var scale = originalScale * gesture.scale
+			if scale < 0.0 {
+				scale = 0.01
+			}
+			model.scale = scale
+			
+			layout.itemSize = computeItemSize()
+			layout.invalidateLayout()
+			
+			if let value = originalValue {
+				scrollToValue(value)
+			}
+			
+			valueDidChange?()
+		}
+	}
+	
+	func computeItemSize() -> CGSize {
+		return CGSize(width: model.scaleRounded, height: PrecisionSlider_InnerModel.height)
+	}
+	
+	lazy var layout: PrecisionSlider_InnerCollectionViewFlowLayout = {
+		let instance = PrecisionSlider_InnerCollectionViewFlowLayout()
+		instance.scrollDirection = .Horizontal
+		instance.minimumInteritemSpacing = 0
+		instance.minimumLineSpacing = 0
+		instance.sectionInset = UIEdgeInsetsZero
+		instance.headerReferenceSize = CGSizeZero
+		instance.footerReferenceSize = CGSizeZero
+		instance.itemSize = self.computeItemSize()
+		instance.model = self.model
+		return instance
+	}()
+	
+	lazy var collectionView: UICollectionView = {
+		let instance = UICollectionView(frame: CGRectZero, collectionViewLayout: self.layout)
+		instance.showsHorizontalScrollIndicator = false
+		instance.showsVerticalScrollIndicator = false
+		instance.backgroundColor = UIColor.blackColor()
+		instance.bounces = false
+		instance.alwaysBounceHorizontal = true
+		instance.alwaysBounceVertical = false
+		instance.registerClass(PrecisionSlider_InnerCollectionViewCell.self, forCellWithReuseIdentifier: PrecisionSlider_InnerCollectionViewCell.identifier)
+		instance.contentInset = UIEdgeInsetsZero
+		instance.delegate = self
+		instance.dataSource = self
+		return instance
+	}()
+	
+	func scrollViewDidScroll(scrollView: UIScrollView) {
+		valueDidChange?()
+	}
+	
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return model.count
+	}
+	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(PrecisionSlider_InnerCollectionViewCell.identifier, forIndexPath: indexPath) as! PrecisionSlider_InnerCollectionViewCell
+		cell.label.text = String(indexPath.row % 10)
+		return cell
+	}
+}
+
+
 public class PrecisionSliderCellModel {
 	var title: String = ""
 	var value: Float = 0.0
@@ -45,6 +285,24 @@ public class PrecisionSliderCellExpanded: UITableViewCell, CellHeightProvider {
 	weak var collapsedCell: PrecisionSliderCell?
 
 	public func form_cellHeight(indexPath: NSIndexPath, tableView: UITableView) -> CGFloat {
-		return 160
+		return PrecisionSlider_InnerModel.height
+	}
+	
+	lazy var sliderView: PrecisionSliderView = {
+		return PrecisionSliderView()
+	}()
+	
+	public init() {
+		super.init(style: .Default, reuseIdentifier: nil)
+		addSubview(sliderView)
+	}
+	
+	required public init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		sliderView.frame = bounds
 	}
 }
