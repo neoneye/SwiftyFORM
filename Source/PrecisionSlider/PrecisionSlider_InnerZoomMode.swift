4b529b29ccf999658d7533e81fe5633296ba7e4e
Simon Strandgaard <neoneye@gmail.com>
2016-07-29 19:34:40 +0200

    Added ZoomMode.ZoomIn(count: 10000)

--- Source/PrecisionSlider/PrecisionSlider_InnerZoomMode.swift
+++ Source/PrecisionSlider/PrecisionSlider_InnerZoomMode.swift
@@ -18,23 +18,24 @@ enum PrecisionSlider_InnerZoomMode {
 	}
 	
 	static func create(scale: Double) -> PrecisionSlider_InnerZoomMode {
-		if scale > 80000      { return .ZoomIn(count: 2000) }
-		if scale > 25000      { return .ZoomIn(count: 1000) }
-		if scale >  9000      { return .ZoomIn(count: 200) }
-		if scale >  2500      { return .ZoomIn(count: 100) }
-		if scale >   800      { return .ZoomIn(count: 20) }
-		if scale >   250      { return .ZoomIn(count: 10) }
-		if scale >   100      { return .ZoomIn(count: 2) }
-		if scale >    30      { return .None }
-		if scale >    10      { return .ZoomOut(count: 5) }
-		if scale >     3      { return .ZoomOut(count: 10) }
-		if scale >     0.6    { return .ZoomOut(count: 50) }
-		if scale >     0.3    { return .ZoomOut(count: 100) }
-		if scale >     0.15   { return .ZoomOut(count: 500) }
-		if scale >     0.032  { return .ZoomOut(count: 1000) }
-		if scale >     0.004  { return .ZoomOut(count: 5000) }
-		if scale >     0.0025 { return .ZoomOut(count: 10000) }
-		if scale >     0.0004 { return .ZoomOut(count: 50000) }
+		if scale > 250000      { return .ZoomIn(count: 10000) }
+		if scale >  80000      { return .ZoomIn(count: 2000) }
+		if scale >  25000      { return .ZoomIn(count: 1000) }
+		if scale >   9000      { return .ZoomIn(count: 200) }
+		if scale >   2500      { return .ZoomIn(count: 100) }
+		if scale >    800      { return .ZoomIn(count: 20) }
+		if scale >    250      { return .ZoomIn(count: 10) }
+		if scale >    100      { return .ZoomIn(count: 2) }
+		if scale >     30      { return .None }
+		if scale >     10      { return .ZoomOut(count: 5) }
+		if scale >      3      { return .ZoomOut(count: 10) }
+		if scale >      0.6    { return .ZoomOut(count: 50) }
+		if scale >      0.3    { return .ZoomOut(count: 100) }
+		if scale >      0.15   { return .ZoomOut(count: 500) }
+		if scale >      0.032  { return .ZoomOut(count: 1000) }
+		if scale >      0.004  { return .ZoomOut(count: 5000) }
+		if scale >      0.0025 { return .ZoomOut(count: 10000) }
+		if scale >      0.0004 { return .ZoomOut(count: 50000) }
 		return .ZoomOut(count: 100000)
 	}
 	
@@ -56,7 +57,7 @@ enum PrecisionSlider_InnerZoomMode {
 				} else {
 					return .Minor
 				}
-			case 10, 100, 1000:
+			case 10, 100, 1000, 10000:
 				if index % 10 == 0 {
 					return .Major
 				}
@@ -101,7 +102,7 @@ enum PrecisionSlider_InnerZoomMode {
 					return nil
 				}
 				return modulo10String(index / 2)
-			case 10, 100, 1000:
+			case 10, 100, 1000, 10000:
 				if index % 10 != 0 {
 					return nil
 				}
