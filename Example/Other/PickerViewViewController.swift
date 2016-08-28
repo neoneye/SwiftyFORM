ed1dc2da2532b23fce34bc475f629a1f15820c27
Simon Strandgaard <neoneye@gmail.com>
2016-08-28 12:06:42 +0200

    Simplified example

--- Example/Other/PickerViewViewController.swift
+++ Example/Other/PickerViewViewController.swift
@@ -1,12 +1,9 @@
 // MIT license. Copyright (c) 2016 SwiftyFORM. All rights reserved.
-import UIKit
 import SwiftyFORM
 
 class PickerViewViewController: FormViewController {
 	override func populate(builder: FormBuilder) {
 		builder.navigationTitle = "PickerViews"
-		builder.toolbarMode = .None
-		
 		builder += SectionHeaderTitleFormItem().title("PickerView")
 		builder += picker0
 		builder += picker1
@@ -14,20 +11,20 @@ class PickerViewViewController: FormViewController {
 	}
 	
 	lazy var picker0: PickerViewFormItem = {
-		let instance = PickerViewFormItem().title("1 component").behavior(.ExpandedAlways)
-		instance.pickerTitles = [["c0 r0", "c0 r1"]]
+		let instance = PickerViewFormItem().title("1 component").behavior(.Expanded)
+		instance.pickerTitles = [["0", "1", "2", "3", "4", "5", "6"]]
 		return instance
 	}()
-
+	
 	lazy var picker1: PickerViewFormItem = {
-		let instance = PickerViewFormItem().title("2 components").behavior(.ExpandedAlways)
-		instance.pickerTitles = [["c0 r0", "c0 r1"], ["c1 r0", "c1 r1"]]
+		let instance = PickerViewFormItem().title("2 components")
+		instance.pickerTitles = [["00", "01", "02", "03"], ["10", "11", "12", "13", "14"]]
 		return instance
 	}()
-
+	
 	lazy var picker2: PickerViewFormItem = {
-		let instance = PickerViewFormItem().title("3 components").behavior(.ExpandedAlways)
-		instance.pickerTitles = [["c0 r0", "c0 r1"], ["c1 r0", "c1 r1"], ["c2 r0", "c2 r1"]]
+		let instance = PickerViewFormItem().title("3 components")
+		instance.pickerTitles = [["00", "01"], ["10", "11"], ["20", "21"]]
 		return instance
 	}()
 }
