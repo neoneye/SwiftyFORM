ab70b875ffd76ffbc8269c3532d00c6710c823c9
Simon Strandgaard <neoneye@gmail.com>
2016-09-17 18:03:56 +0200

    Described in greater detail what is going on when the header/footer is to be zero height

--- Source/Form/TableViewSectionPart.swift
+++ Source/Form/TableViewSectionPart.swift
@@ -28,7 +28,7 @@ public enum TableViewSectionPart {
 	var height: CGFloat {
 		switch self {
 		case .none:
-			return CGFloat.leastNonzeroMagnitude
+			return 0
 		case .systemDefault:
 			return UITableViewAutomaticDimension
 		case let .titleView(view):
@@ -41,6 +41,12 @@ public enum TableViewSectionPart {
 	var estimatedHeight: CGFloat {
 		switch self {
 		case .none:
+			/**
+			We don't want any header/footer to be visible. The height should be zero pixels.
+			However returning an estimated height smaller than 2 pixels 
+			causes UITableView to think that the height is 44 pixels
+			Thus the lowest estimated height we can return is 2 pixels
+			*/
 			return 2
 		case .systemDefault:
 			return UITableViewAutomaticDimension
