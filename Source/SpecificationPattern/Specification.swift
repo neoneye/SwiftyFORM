737b0a3c4d7d409d5268df2bfcf49ed7f4f1bdd4
Simon Strandgaard <neoneye@gmail.com>
2016-10-19 22:46:51 +0200

    Writing documentation

--- Source/SpecificationPattern/Specification.swift
+++ Source/SpecificationPattern/Specification.swift
@@ -1,19 +1,55 @@
 // MIT license. Copyright (c) 2016 SwiftyFORM. All rights reserved.
 
 /// A type that can check whether or not a candidate object satisfy the rules.
+///
+/// Specifications are cheap to write, easy to test and can be combined to
+/// represent very complex business rules.
 public protocol Specification {
+	
+	/// The central part of a specification is the `isSatisfiedBy()` function, 
+	/// which is used to check if an object satisfies the specification.
+	///
+	/// - parameter candidate: The object to be checked.
+	///
+	/// - returns: `true` if the candidate satisfies the specification, `false` otherwise.
 	func isSatisfiedBy(_ candidate: Any?) -> Bool
+	
 }
 
 extension Specification {
+
+	/// Combine two specifications into one.
+	///
+	/// This is an **AND** operation. The new specification is satisfied
+	/// when both specifications are satisfied.
+	///
+	/// - parameter other: The other specification that is to be to combine with this specification.
+	///
+	/// - returns: A combined specification
 	public func and(_ other: Specification) -> Specification {
 		return AndSpecification(self, other)
 	}
 	
+
+	/// Combine two specifications into one.
+	///
+	/// This is an **OR** operation. The new specification is satisfied
+	/// when either of the specifications are satisfied.
+	///
+	/// - parameter other: The other specification that is to be to combine with this specification.
+	///
+	/// - returns: A combined specification
 	public func or(_ other: Specification) -> Specification {
 		return OrSpecification(self, other)
 	}
 	
+
+	/// Invert a specification.
+	///
+	/// This is a **NOT** operation. The new specification is satisfied
+	/// when the specification is not satisfied.
+	///
+	/// - returns: An inverted specification
 	public func not() -> Specification {
 		return NotSpecification(self)
 	}
