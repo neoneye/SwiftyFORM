43d8f9a92fb8bf939300b762df2dcfd424ab4911
Simon Strandgaard <neoneye@gmail.com>
2016-10-19 23:28:34 +0200

    Writing documentation

--- Source/SpecificationPattern/PredicateSpecification.swift
+++ Source/SpecificationPattern/PredicateSpecification.swift
@@ -1,13 +1,21 @@
 // MIT license. Copyright (c) 2016 SwiftyFORM. All rights reserved.
 import Foundation
 
+/// Check if a object is satisfied by a closure.
+///
+/// Closure is sometimes preferred instead of subclassing the `Specification` protocol.
 public class PredicateSpecification<T>: Specification {
-	public let predicate: (T) -> Bool
+	private let predicate: (T) -> Bool
 	
-	init(predicate: @escaping (T) -> Bool) {
+	public init(predicate: @escaping (T) -> Bool) {
 		self.predicate = predicate
 	}
 	
+	/// Check if the closure is satisfied.
+	///
+	/// - parameter candidate: The object to be checked.
+	///
+	/// - returns: `true` if the candidate object is of right type and is satisfied by the closure, `false` otherwise.
 	public func isSatisfiedBy(_ candidate: Any?) -> Bool {
 		guard let obj = candidate as? T else { return false }
 		return predicate(obj)
--- Source/SpecificationPattern/RegularExpressionSpecification.swift
+++ Source/SpecificationPattern/RegularExpressionSpecification.swift
@@ -1,8 +1,16 @@
 // MIT license. Copyright (c) 2016 SwiftyFORM. All rights reserved.
 import Foundation
 
+/// Check if a string satisfies a regular expression (AKA. regex).
+///
+/// [Regex tutorial](http://www.regular-expressions.info/quickstart.html)
+///
+/// Regex are really powerful for validating textfields. Here a business rule could be
+/// that the text must be like this: `aabbcc8`. A regex for this:
+///
+/// `let spec = RegularExpressionSpecification(pattern: "^a+b+c+\\d$")`
 public class RegularExpressionSpecification: Specification {
-	public let regularExpression: NSRegularExpression
+	private let regularExpression: NSRegularExpression
 	
 	public init(regularExpression: NSRegularExpression) {
 		self.regularExpression = regularExpression
@@ -13,6 +21,11 @@ public class RegularExpressionSpecification: Specification {
 		self.init(regularExpression: regularExpression)
 	}
 	
+	/// Check if the regex is satisfied.
+	///
+	/// - parameter candidate: The object to be checked.
+	///
+	/// - returns: `true` if the candidate object is a `String` and it satisfies the regex, `false` otherwise.
 	public func isSatisfiedBy(_ candidate: Any?) -> Bool {
 		guard let s = candidate as? String else { return false }
 		return regularExpression.numberOfMatches(in: s, options: [], range: NSMakeRange(0, s.characters.count)) > 0
