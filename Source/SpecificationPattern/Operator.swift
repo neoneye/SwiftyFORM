60bf3f4afe242fc810f8b9b0794e4e99488b3122
Simon Strandgaard <neoneye@gmail.com>
2016-10-19 21:29:42 +0200

    Writing documentation

--- Source/SpecificationPattern/Operator.swift
+++ Source/SpecificationPattern/Operator.swift
@@ -1,44 +1,84 @@
-/*
-And operator - shorthand for .and()
+// MIT license. Copyright (c) 2016 SwiftyFORM. All rights reserved.
 
-USAGE:
-let spec = onlyDigits & between2And4Letters & modulus13Checksum
+/**
+AND operator
+
+Combine two specifications into a single specification.
+
+This is a shorthand for the `Specification.and()` function.
+
+Both the `left` specifiction and the `right` specifiction must be satisfied.
+
+## Example
+
+`let spec = onlyDigits & between2And4Letters & modulus13Checksum`
 */
 public func & (left: Specification, right: Specification) -> Specification {
 	return left.and(right)
 }
 
-/*
-Or operator - shorthand for .or()
 
-USAGE:
-let spec = connectionTypeWifi | connectionType4G | hasOfflineData
+/**
+OR operator
+
+Combine two specifications into a single specification.
+
+This is a shorthand for the `Specification.or()` function.
+
+Either the `left` specifiction or the `right` specifiction must be satisfied.
+
+## Example
+
+`let spec = connectionTypeWifi | connectionType4G | hasOfflineData`
 */
 public func | (left: Specification, right: Specification) -> Specification {
 	return left.or(right)
 }
 
-/*
-Negate operator - shorthand for .not()
 
-USAGE:
-let spec = ! filesystemIsFull
+/**
+Negate operator
+
+This is a shorthand for the `Specification.not()` function.
+
+This specifiction is satisfied, when the given specification is not satisfied.
+
+This specifiction is not satisfied, when the given specification is satisfied.
+
+## Example
+
+`let spec = ! filesystemIsFull`
 */
 public prefix func ! (specification: Specification) -> Specification {
 	return specification.not()
 }
 
 
-/*
-Equivalence operators - shorthand for .isSatisfiedBy()
+/**
+Equivalence operator 
 
-USAGE:
-spec == "123"
-spec != "123"
+This is a shorthand for the `Specification.isSatisfiedBy()` function
+
+## Example:
+
+`let spec = CharacterSetSpecification.decimalDigitCharacterSet()`
+`spec == "123"`
 */
 public func == (left: Specification, right: Any?) -> Bool {
 	return left.isSatisfiedBy(right)
 }
+
+
+/**
+Not equivalent operator
+
+This is a shorthand for the `Specification.isSatisfiedBy()` function
+
+## Example:
+
+`let spec = CharacterSetSpecification.decimalDigitCharacterSet()`
+`spec != "123"`
+*/
 public func != (left: Specification, right: Any?) -> Bool {
 	return !left.isSatisfiedBy(right)
 }
