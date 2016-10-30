ab71042637a44599c0302c1aa0c3dbc549e3c704
Simon Strandgaard <neoneye@gmail.com>
2016-10-30 02:33:02 +0200

    Poor mans solution to the UITableViewDelegate.tableView(titleForHeaderInSection) with multi line texts

--- Source/Form/TableViewSectionPart.swift
+++ Source/Form/TableViewSectionPart.swift
@@ -57,8 +57,39 @@ public enum TableViewSectionPart {
 			Solution is to ensure that the value never drops below 10 pixels.
 			*/
 			return max(view.frame.height, 10)
-		case .titleString(_):
-			return 44
+		case let .titleString(string):
+			/**
+			Argh, we cannot simply return UITableViewAutomaticDimension
+			If we do then sections titles will show up with the wrong heights.
+			
+			In order for UITableView to automatically figure the height, we must return 44 pixels.
+			Apple's default estimate is 44 pixels when data is coming from
+			UITableViewDelegate.tableView(titleForHeaderInSection)
+			So we have to return 44 pixels, then UITableView will assign a suitable
+			size to the section header.
+			
+			UITableView works best with section titles that spans just a single line.
+			However with multiline titles then expand/collapse animations becomes crash prone.
+			So with longer texts then the estimate must be greater than 44 pixels.
+
+			Poor mans solution for now
+			short texts then return 44
+			long texts then return 88
+			
+			The right solution would be to consider font size, screen size for determining the label height.
+			This may be overfitting the problem.
+			*/
+			if string.characters.count > 50 {
+				print("WARNING: Multiline section texts may cause crashes, consider instead using SectionHeaderViewFormItem")
+				return Constant.estimatedHeightForTitleSection * 2
+			}
+			return Constant.estimatedHeightForTitleSection
 		}
+	}
+
+	struct Constant {
+		/// Default height of a section header with a title
+		/// Apple doesn't seem to have any constant for this
+		static let estimatedHeightForTitleSection: CGFloat = 44
 	}
 }
