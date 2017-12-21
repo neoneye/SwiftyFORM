3c9878c3d69715b01dc265a1cdf5a29973cb501f
Simon Strandgaard <neoneye@gmail.com>
2017-12-21 11:08:45 +0000

    Reintroduced the swift4 code that I just removed earlier and added #if swift4 condition, so that things still compiles with swift3

--- Source/FormItems/AttributedTextFormItem.swift
+++ Source/FormItems/AttributedTextFormItem.swift
@@ -16,15 +16,16 @@ public class AttributedTextFormItem: FormItem {
 
 	@discardableResult
 	public func title(_ title: String, _ attributes: [String: AnyObject]? = nil) -> Self {
-		if let attributes = attributes {
-			var newAttributes = [NSAttributedStringKey: Any]()
-			for (key, value) in attributes {
-				newAttributes[NSAttributedStringKey(key)] = value
+		#if swift(>=4.0)
+			if let attributes = attributes {
+				let newAttributes = Dictionary(uniqueKeysWithValues: attributes.map { key, value in (NSAttributedStringKey(key), value) })
+				self.title = NSAttributedString(string: title, attributes: newAttributes)
+			} else {
+				self.title = NSAttributedString(string: title, attributes: nil)
 			}
-			self.title = NSAttributedString(string: title, attributes: newAttributes)
-		} else {
-			self.title = NSAttributedString(string: title, attributes: nil)
-		}
+		#else
+			self.title = NSAttributedString(string: title, attributes: attributes)
+		#endif
 		return self
 	}
 
@@ -52,15 +53,16 @@ public class AttributedTextFormItem: FormItem {
 
 	@discardableResult
 	public func value(_ value: String, _ attributes: [String: AnyObject]? = nil) -> Self {
-		if let attributes = attributes {
-			var newAttributes = [NSAttributedStringKey: Any]()
-			for (key, value) in attributes {
-				newAttributes[NSAttributedStringKey(key)] = value
+		#if swift(>=4.0)
+			if let attributes = attributes {
+				let newAttributes = Dictionary(uniqueKeysWithValues: attributes.map { key, value in (NSAttributedStringKey(key), value) })
+				self.value = NSAttributedString(string: value, attributes: newAttributes)
+			} else {
+				self.value = NSAttributedString(string: value, attributes: nil)
 			}
-			self.value = NSAttributedString(string: value, attributes: newAttributes)
-		} else {
-			self.value = NSAttributedString(string: value, attributes: nil)
-		}
+		#else
+			self.value = NSAttributedString(string: value, attributes: attributes)
+		#endif
 		return self
 	}
 }
