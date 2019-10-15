7eb7f1216f1c4011ad06478e4a03a200fc2b5335
Bradley Mackey <bradleymackey@me.com>
2019-10-15 13:07:04 +0100

    fix insets ios 11

--- Source/Cells/TextFieldCell.swift
+++ Source/Cells/TextFieldCell.swift
@@ -189,7 +189,10 @@ public class TextFieldFormItemCell: UITableViewCell, AssignAppearance {
 		var cellHeight: CGFloat = 0
 		let veryTallCell = CGRect(x: 0, y: 0, width: cellWidth, height: CGFloat.greatestFiniteMagnitude)
 
-        var layoutMargins = self.safeAreaInsets
+        var layoutMargins = self.layoutMargins
+        if #available(iOS 11, *) {
+            layoutMargins = self.safeAreaInsets
+        }
 		layoutMargins.top = 0
 		layoutMargins.bottom = 0
 		let area = veryTallCell.inset(by: layoutMargins)
